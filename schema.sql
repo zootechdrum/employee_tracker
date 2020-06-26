117dbd77725a936109d9ba33fd0e8e18f2f0ad6a
Cesar Gomez <zootechdrum@Cesars-MacBook-Pro.local>
2020-06-25 19:22:54 -0700

    added department table and role table to schema file

--- schema.sql
+++ schema.sql
@@ -2,3 +2,16 @@ DROP DATABASE IF EXISTS employee_tracker;
 CREATE database employee_tracker;
 
 USE employee_tracker;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(100) NULL
+    PRIMARY KEY (id)
+)
+
+Create TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30)
+    salary
+    PRIMARY KEY (id)
+)
