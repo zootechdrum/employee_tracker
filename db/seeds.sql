9698b5beba47dee0582a2ad9144b32a16c1d0fc9
Cesar Gomez <zootechdrum@Cesars-MacBook-Pro.local>
2020-06-28 09:33:12 -0700

    fixed query for view all employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
 INSERT INTO Department (department_name) VALUES
 ('Quality-Assurance'),('transportation');
 
-INSERT INTO Role ( title, salary ) VALUES
-("Guitar-Inspector", 8000.00),
-("Transportation-Cordinator" , 7000.00);
+INSERT INTO Role ( title, salary,department_id ) VALUES
+("Guitar-Inspector", 8000.00, 1),
+("Transportation-Cordinator" , 7000.00, 2);
 
 INSERT INTO Employee ( first_name, last_name, role_id, manager_id  ) VALUES
 ("Steve", "Cornell", 2, NULL),
